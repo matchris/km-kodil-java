3996ce2c8cbe4bb05ea66bbeb74ed719f9ed3129
matchris <krzysztof.matyjaszek@outlook.com>
2020-03-13 12:49:07 +0100

    ArrayList<Integer> evenList added as local variable of exterminate method

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -3,9 +3,8 @@ package com.kodilla.testing.collection;
 import java.util.ArrayList;
 
 public class OddNumbersExterminator {
-    ArrayList<Integer> evenList = new ArrayList<>();
-
     public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
+        ArrayList<Integer> evenList = new ArrayList<>();
         for (Integer element : numbers) {
             if (element % 2 == 0) {
                 evenList.add(element);
