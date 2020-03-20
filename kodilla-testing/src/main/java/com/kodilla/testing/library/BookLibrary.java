51b57eb593a2b5a5bc0abf2e827692b1fc3124f0
matchris <krzysztof.matyjaszek@outlook.com>
2020-03-20 11:28:48 +0100

    Book directory Test Suite 6.6 v.2

--- kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -12,7 +12,6 @@ public class BookLibrary {
     }
 
     public List<Book> listBooksInHandsOf(LibraryUser libraryUser){
-        List<Book> booksInHands = new ArrayList<>();
         return libraryDatabase.listBooksInHandsOf(libraryUser);
     }
 
