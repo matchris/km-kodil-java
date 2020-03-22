f8d22f689b8c0f27f5b8d78da6e909827eede6e1
matchris <krzysztof.matyjaszek@outlook.com>
2020-03-22 14:49:23 +0100

    Recovered version after deleted some files

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -13,5 +14,8 @@ public class StreamMain {
         poemBeautifier.beautify("Butterfly", (someString) -> someString.length() + " it's our " + someString + " string length");
         System.out.println("Beautifying strings with lambdas by lower casing");
         poemBeautifier.beautify("SOME FLOWER BLOOMS OVER OUR HEADS ", (someString) -> someString.toLowerCase()+ " before decorated: "  + someString);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
