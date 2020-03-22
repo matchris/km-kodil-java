8bb7c734f53234be5b704f5f8328d8bf5c39222c
matchris <krzysztof.matyjaszek@outlook.com>
2020-03-22 16:50:21 +0100

    ForumUser 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
@@ -4,6 +4,9 @@ import java.util.stream.Stream;
 
 public class NumbersGenerator {
     public static void generateEven(int max) {
-        Stream.iterate(1, n -> n + 1).limit(max).filter(n->n%2==0).forEach(System.out::println);
+        Stream.iterate(1, n -> n + 1)
+                .limit(max)
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
     }
 }
